890bb0b61974cba9081f50beb16115a087b851b0
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2021-12-20 19:49:14 +0100

    función 1

--- EstrellasRegulares.py
+++ EstrellasRegulares.py
@@ -1,2 +1,13 @@
 import turtle
 
+def dibujaEstrella(size, points, angle):
+    angle2= 360/points+angle
+
+    for x in range(points*2):
+        turtle.forward(size)
+        if x%2==0:
+            turtle.left(180-angle)
+        else:
+            turtle.right(180-angle2)
+        return
+        
